b1d2bc2e5988092694a14bfc9be291e10e67f68b
Ion19 <engabdelrhman2016@gmail.com>
2019-03-19 22:30:53 +0200

    upload build file

---
+++ build/precache-manifest.1ff379036be7771ad2217696c0fba4f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30dc9e10760a4a624dab",
+    "url": "/static/js/main.cb624f70.chunk.js"
+  },
+  {
+    "revision": "ec150f2bad8367e24837",
+    "url": "/static/js/2.aaf62a5c.chunk.js"
+  },
+  {
+    "revision": "cb43e19531c799eb0ea66c8207189d89",
+    "url": "/index.html"
+  }
+];
